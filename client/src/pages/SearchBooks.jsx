b6f0358f8f3068a4a42425b57d7c2b950a0954da
MeganKR <megankaye2018@gmail.com>
2024-02-28 10:02:50 -0600

    setup apollo and book search functionality

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -1,4 +1,7 @@
 import { useState, useEffect } from 'react';
+import { useMutation } from '@apollo/client';
+import { Link } from 'react-router-dom';
+import { SAVE_BOOK } from '../utils/mutations';
 import {
   Container,
   Col,
@@ -72,7 +75,11 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const { data } = await saveBookMutation({
+        variables: { bookInput: bookToSave }
+      });
+
+      setSavedBookIds([...savedBookIds, data.saveBook.id]);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
